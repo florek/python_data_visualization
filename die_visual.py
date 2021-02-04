8b726f34702166d578d5742160e557398014e987
Florek <pawel.florczak87@gmail.com>
2021-02-04 17:51:49 +0100

    04.02.2021

---
+++ die_visual.py
@@ -0,0 +1,25 @@
+from plotly.graph_objs import Bar, Layout
+from plotly import offline
+from die import Die
+
+die = Die()
+
+results = []
+
+for roll_num in range(1000):
+    result = die.roll()
+    results.append(result)
+
+frequencies = []
+for value in range(1, die.num_sides + 1):
+    frequency = results.count(value)
+    frequencies.append(frequency)
+
+x_values = list(range(1, die.num_sides + 1))
+data = [Bar(x=x_values, y=frequencies)]
+
+x_axis_config = {'title': 'Wynik'}
+y_axis_config = {'title': 'Częstotliwość występowania wartości'}
+
+my_layout = Layout(title='Wynik rzucania pojedynczą kością D6 tysiąc razy', xaxis=x_axis_config, yaxis=y_axis_config)
+offline.plot({'data': data, 'layout': my_layout}, filename='d6.html')
